1bb95a68959198cefb61f7d029edcc2187dab7f4
ashish sharma <ashishsharmastudio@gmail.com>
2024-10-28 00:26:59 +0530

    first commit

--- stock-market-api/src/app.js
+++ stock-market-api/src/app.js
@@ -41,34 +41,3 @@ app.listen(PORT, () => {
 });
 
 module.exports = app;
-
-// const express = require("express");
-// const cors = require("cors");
-// const dotenv = require("dotenv");
-// const morgan = require("morgan");
-// const connectDB = require("./config/database");
-// const stockRoutes = require("./routes/stockRoutes");
-// const uploadRoutes = require("./routes/uploadRoutes");
-
-// dotenv.config();
-
-// const app = express();
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-// app.use(morgan("dev"));
-
-// // Connect to MongoDB
-// connectDB();
-
-// // Routes
-// app.use("/api", stockRoutes);
-// app.use("/upload", uploadRoutes);
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-// module.exports = app;
